804495be6365488c967789c8149542bd16cdf6ae
goz1985 <isaackegs@gmail.com>
2022-03-30 22:20:32 +0300

    Apriori model, fine tunned the model to get a different number of rules in relation to the confidence and support

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Machine learning interpretability is an important concept in the world of data analytics. How can we be able to trust the decisions made on our data by the models we employ on them.
 The research seeks to use Rough Sets which is an information granulation and feature selection method to sift through attributes of any dataset, then model the selected attributes using apirori association rule mining method to find objectives rule from the selected attributes.
 The main intention is to make the user see the kind of attribute sthe model is choosing for it's prediction purposes.
+The reserach starts off with laoding of the datasets and preprocessing them to ensure that the data is clean and free from errors or omissions.
+Preprocessing was the main hurdle in this research especially the kirogo datasets which had 30min interval weather reading.
+The structure of the datasets are as follows:
+Kirogo attributes were: Date/Time;RH(%),Temp(Degrees celcius), Rainfall
+
+Next I run sample machine learning models on the dataset to get insights on the data, this is important as it will form the basis for comparing my proposed model performance the standard machine learning models i will have choosen for the study
--- RST_ARM_GLM Experiments/ROUGHSET_Model.R
+++ RST_ARM_GLM Experiments/ROUGHSET_Model.R
@@ -14,7 +14,7 @@ kariki_farm <- read.csv("E:/Datasets/Research datasets/Weather data/Kariki_Farm.
 (n<-nrow(kariki_farm)) # Checking number of rows in the data which is 1179
 c(as.character(kariki_farm$Date[1]), as.character(kariki_farm$Date[n])) # the date range from 2/3/2018 to 20/5/2021
 head(kariki_farm$Rain.Yes.No.)
-kariki_farm$Rain <- factor(kariki_farm$Rain) # Rain Yes/No to factor
+kariki_farm$Rain <- factor(kariki_farm$Rain, labels = c("No","Yes"))# Rain Yes/No to factor
 kariki_farm$Date <- as.Date(kariki_farm$Date, '%m/%d/%Y') # Date column to date
 str(kariki_farm)# When looking at the high and low numeric pressure, I had to convert them to numeric because they were being considered as being factors yet theyr weren't
 
@@ -42,7 +42,118 @@ library(Hmisc) #Giving an error message while loading
 kariki_imputed <- mice(kariki_farm, m =1, method = "pmm", maxit = 50, seed = 123)
 kariki_imputed_complete <- complete(kariki_imputed)
 
+#Impute with the HMISC package...This package not loading
+
+# If not imputing will be removing the rows with missing data
+(cols_withNa <- apply(kariki_farm, 2, function(x) sum(is.na(x))))
+kariki_farm2 <- kariki_farm[complete.cases(kariki_farm),]
+str(kariki_farm2)
+(cols_withNa <- apply(kariki_farm2, 2, function(x) sum(is.na(x))))
+kariki_farm2$Date <- NULL # removing the date column
+#Remove windspeed as it doesnt add any value to our research
+kariki_farm2$Windspeed_low <-NULL
+
+view(kariki_farm2)
+
 ##### Dealing with the roughset
-# 
+library(arules)
+library(dplyr)
+
+# When dealing with RST technology:
+#' 1. Create a decision table from the given dataframe
+#' 2. Discretize the values which may be numeric into nominal values using the RST methods of global, local and unsupervised
+#' 3. Feature selection
+
+kariki_shuffled <- kariki_farm2[sample(nrow(kariki_farm2)),]
+kariki_DT <- SF.asDecisionTable(kariki_shuffled, decision.attr = 15, indx.nominal = c(1:14)) # Indx.nominal is picking conditional attributes
+####kariki_DT_cutValues <- D.discretization.RST(kariki_DT, type.method = "local.discernibility")## Saying all conditional attributes are nominal
+
+# Computing the indiscernibility relation
+IND <- BC.IND.relation.RST(kariki_DT)
+
+# Compute Upper and Lower Approximations
+kariki_roughset <- BC.LU.approximation.RST(kariki_DT,IND)
+
+# Determining the regions
+kariki.regions <- BC.positive.reg.RST(kariki_DT,kariki_roughset)
+
+# Discernibility matrix formulation
+kariki.matrix <- BC.discernibility.mat.RST(kariki_DT)
+
+# Reduct formulation using
+#' 1 Greedy heuristic
+GH_Kariki_reduct <- FS.greedy.heuristic.reduct.RST(kariki_DT,qualityF = X.entropy,epsilon = 0.0)
+GH_Table <- SF.applyDecTable(kariki_DT,GH_Kariki_reduct)
+# With this method we get a reduct made up of 4 variables: High_temp, Dew_point, Precipitation amount and Rain
+
+# Analysis of the reduct using various Ml Models to compare with the other
+
+############################Now applying several machine learning models on the data to see what it will output
+library(tidyverse)
+library(caret)
+library(knitr)
+library(lattice)
+library(grid)
+library(gridExtra)
+library(ROCR)
+library(corrplot)
+library(caretEnsemble)
+library(fastAdaboost)
+set.seed(123)
+GH_kariki_train <- createDataPartition(GH_Table$Rain, p =0.75, list = FALSE)
+GH_training <- GH_Table[GH_kariki_train,]
+GH_testing <- GH_Table[-GH_kariki_train,]
+
+#### Ensemble methods#########
+GH_control <- trainControl(method="repeatedcv", number=10, repeats=3,
+                        savePredictions=TRUE, classProbs=TRUE,preProc = c("center","scale"))
+
+## TreeBag Method, and running the testing data on the developed model####
+seed <- 123
+metric <- "Accuracy"
+set.seed(seed)
+fit.treebag_GH <- train(Rain~., data=GH_training, method="treebag", metric=metric, trControl=GH_control)
+
+# Running the predictions..the model on the reduct gives an accuracy of 89%
+predictions_treebag_GH<-predict(object=fit.treebag_GH ,GH_testing, type="raw")
+table(predictions_treebag_GH)
+confusionMatrix(predictions_treebag_GH,GH_testing$Rain)
+
+### Running on the random forest model#### Was taking to much time to run
+set.seed(seed)
+fit.rf_GH <- train(Rain~., data=GH_training, method="rf", metric=metric, trControl=GH_control)
+
+predictions_rf_GH<-predict(object=fit.rf_GH ,GH_testing, type="raw")
+table(predictions_rf_GH)
+confusionMatrix(predictions_rf_GH,GH_testing$Rain)
+
+
+
+####Applying the association rule mining method to get the decision rules
+
+
+#Convert DT to transactions, this helps in identifying how attributes/features are related to each other
+GH_Table_Frame <- as.data.frame(GH_Table) # Coerce Decision table back to dataframe before coercing them to transactions
+kariki_trans_GH<-as(GH_Table_Frame,"transactions")
+
+itemLabels(kariki_trans_GH)
+image(kariki_trans_GH)
+
+#Generating the rule susing apriori method
+kariki_rules <- apriori(kariki_trans_GH)
+
+
+
+
+
+
+#' 2. Super reduct method
+# Using the discernibility matrix kariki.matrix to formulate a reduct using all reduct computation
+
+reduct <- FS.all.reducts.computation(kariki.matrix) # Method very time consuming..check system time computation
+#The method henerates 477 reducts..generally not a feasible method as the research i proposed the greedy heuristic method.
+
+
+
 
 
