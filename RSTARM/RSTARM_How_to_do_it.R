1fa954c927c43d70fac50635a9c877213ab11126
Isaac Kega <isaackegs@gmail.com>
2022-05-07 13:09:20 +0300

    Updated the script to remove redundant sections

--- RSTARM/RSTARM_How_to_do_it.R
+++ RSTARM/RSTARM_How_to_do_it.R
@@ -1,25 +1,41 @@
 library(RoughSets)
+#' Load the Rough Set library which comes with the test data set for hiring where a decision maker will decide whether-
+#' to employ a person based on their diploma qualification, reference or experience.
+#' The work involves deducing important attributes from the data set using the indiscernibility matrix
+#' rough sets and discerniblity matrix computations.
 data("RoughSetData")
 attr <- c(1,2,3)
-IND <- BC.IND.relation.RST(decision.table, feature.set = attr.P)
 decision.table<-RoughSetData$hiring.dt
-IND <- BC.IND.relation.RST(decision.table, feature.set = attr.P)
 IND <- BC.IND.relation.RST(decision.table, feature.set = attr)
+#Computation of upper and lower approximations to check with attributes affect the decision variable well and which dont.
+
 roughset <- BC.LU.approximation.RST(decision.table, IND)
 region.RST <- BC.positive.reg.RST(decision.table, roughset)
 disc.mat <- BC.discernibility.mat.RST(decision.table, range.object = NULL)
 decision_table_hiring <- FS.all.reducts.computation(disc.mat)
-new_hiring_table <- SF.applyDecTable(decision.table,decision_table_hiring,control = list(indx.reduct = 1))
-install.packages("arules")
+
+# From the above reduct computation we get two Reduct each having different attributes in the tables
+# Table one the attributes are : "Diploma", "Experience, "Decision"
+# Table two the attributes are : "Experience", "Reference", "Decision"
+new_hiring_table1 <- SF.applyDecTable(decision.table,decision_table_hiring,control = list(indx.reduct = 1))
+new_hiring_table2 <- SF.applyDecTable(decision.table,decision_table_hiring,control = list(indx.reduct = 2))
+
+# Loading the association rule mining package for formulating the decision rules for the above dataset.
+
 library(arules)
-library(arules)
-AR_table <- as.data.frame(new_hiring_table )
-View(AR_table)
-tdata <- as(AR_table,"transactions")
+AR_table_1 <- as.data.frame(new_hiring_table1 )
+View(AR_table_1)
+# Convert the Reducts into transactions. The transactions help identify strong patterns and relationships among the patterns in the datasets.
+tdata <- as(AR_table_1,"transactions")
 View(tdata)
-rules <- apriori(tdata,parameter = list(minlen=2,supp=0.005,conf=0.8),appearance = list(rhs=c("Decision=Reject","Decision=Accept"),default="lhs",control=list(verbose=F)))
+
+#Rule generation using ARM
 rules <- apriori(tdata,parameter = list(minlen=2,supp=0.005,conf=0.8),appearance = list(rhs=c("Decision=Reject","Decision=Accept"),default="lhs"))
 inspect(rules)
+
+# Dataframe for the rules generated
 rules_df <- data.frame(lhs=labels(lhs(rules)),rhs=labels(rhs(rules)))
 View(rules_df)
 rules_df <- data.frame(lhs=labels(lhs(rules)),rhs=labels(rhs(rules)),rules@quality)
+
+# Generating a good binary data frame
