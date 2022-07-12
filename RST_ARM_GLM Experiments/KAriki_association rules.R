37dfbac82ca7a1f37d8ba5254bffe2b55e622a61
Isaac Kega <isaackegs@gmail.com>
2022-07-12 19:40:29 +0300

    Able to prune rules and visualize these rules

--- RST_ARM_GLM Experiments/KAriki_association rules.R
+++ RST_ARM_GLM Experiments/KAriki_association rules.R
@@ -1,11 +1,11 @@
-###### Creating association rules on the data set without taking it through the Roughset Process###########
+###### Creating association rules on the kariki_farm data set without taking it through the Roughset Process###########
 
 library(googlesheets4)
 library(arules)
 library(dplyr)
 library(RColorBrewer)
 library(arulesViz)
-
+library(plyr)
 k_farm <- read_sheet("https://docs.google.com/spreadsheets/d/1y29ch-sv9UXSZUX9mRxqx6NleN6-XO3ifXDqkDzeOiE/edit#gid=0")
 
 (n<-nrow(k_farm)) # Checking number of rows in the data which is 1179
@@ -48,6 +48,47 @@ k_farm$Windspeed_High<-discretize(k_farm$Windspeed_High)
 k_farm$Windspeed_Avg<-discretize(k_farm$Windspeed_Avg)
 k_farm$`High(Hpa)`<-discretize(k_farm$`High(Hpa)`)
 k_farm$`Low(Hpa)`<-discretize(k_farm$`Low(Hpa)`)
+
+
 kfarm_trans <- as(k_farm,"transactions")
 
 itemLabels(kfarm_trans)
+
+itemFrequencyPlot(kfarm_trans,topN=20,type='absolute')
+
+kfarm_freq_item_set <- eclat (kfarm_trans, parameter = list(supp = 0.275, maxlen = 15))
+inspect(kfarm_freq_item_set)
+
+
+#########################Rule generation###################################################
+
+K_rules <- apriori(kfarm_trans,parameter = list(minlen = 2,supp= 0.175, conf = 0.5, maxlen=5),appearance = list(rhs= c("Rain=Yes", "Rain=No")))
+
+k_rules_table <- data.frame(lhs=labels(lhs(K_rules)),rhs=labels(rhs(K_rules)),K_rules@quality)
+
+###Pruning rules which are subset of other rules, originally there were 99 rules, after prunning they are now 38 rules#######
+subset.k_rules<-is.subset(K_rules)
+subset.k_rules
+
+subset.k_rules[lower.tri(subset.k_rules, diag=T)] <- F
+subset.k_rules
+
+redundant<- apply(subset.k_rules,2,any)
+redundant
+
+rules.pruned <- K_rules[!redundant]
+inspect(rules.pruned)
+
+interestMeasure(rules.pruned, c("support", "chiSquare", "confidence", "conviction", "cosine", "coverage", "leverage", "lift", "oddsRatio"), k_farm)
+
+########Visualize the rules via graphs to see the distribution of the rules according to the classes######
+plot(rules.pruned)
+
+plot(rules.pruned, method="grouped")
+
+plot(rules.pruned, method="graph")
+
+######Putting the pruned rules in a dataframe##########################
+kariki_pruned_rules <- data.frame(lhs=labels(lhs(rules.pruned)),rhs=labels(rhs(rules.pruned)),rules.pruned@quality)
+
+write.csv(kariki_pruned_rules,"D:\\Phd Research\\pruned_rules.csv", row.names = FALSE)
